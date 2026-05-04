f1792d06b19e9e257badecba6344f03a26acc06d
Kinike <86970091+gusguela@users.noreply.github.com>
2026-05-04 08:44:43 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,36 +1,13 @@
-function somar(){
-    var n1 = document.getElementById("sum_A").valueAsNumber
-    var n2 = document.getElementById("sum_B").valueAsNumber
-    var ans = document.getElementById("ans_sum")
+function media(){
+    var a = document.getElementById("nota1").valueAsNumber
+    var b = document.getElementById("nota2").valueAsNumber
+    var c = document.getElementById("nota3").valueAsNumber
+    var ans = document.getElementById("resposta")
+    var media = (a + b + c)/3
 
-    
-    ans.textContent = `A Soma dos números é: ${n1 + n2}`
-}
-
-function subt(){
-    var n1 = document.getElementById("sub_A").valueAsNumber
-    var n2 = document.getElementById("sub_B").valueAsNumber
-    var ans = document.getElementById("ans_sub")
-    
-    ans.textContent = `A Subtração dos números é: ${n1 - n2}`
-}
-
-function mult(){
-    var n1 = document.getElementById("mult_A").valueAsNumber
-    var n2 = document.getElementById("mult_B").valueAsNumber
-    var ans = document.getElementById("ans_mult")
-    
-    ans.textContent = `A Multiplicação dos números é: ${n1 * n2}`
-}
-
-function divs(){
-    var n1 = parseFloat(document.getElementById("divs_A").value)
-    var n2 = parseFloat(document.getElementById("divs_B").value)
-    var ans = document.getElementById("ans_divs")
-    
-    if (n2 === 0){
-        ans.textContent = "Não é possível dividir por 0"
+    if (media >= 7){
+        ans.textContent = $[media.toFixed(2)] `- PASSOU DE ANO ✅`
     }else{
-        ans.textContent = `A Divisão dos números é: ${n1 / n2}`
+        ans.textContent = $[media.toFixed(2)] `- REPROVOU DE ANO ❌`
     }
 }
